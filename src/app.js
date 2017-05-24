8311d9faa94b45836bc5bee2936303539caf0158
vynessa <vanessa.ejikeme@gmail.com>
2017-05-24 18:35:32 +0100

    [chore]: Sample for starting with webpack

---
+++ src/app.js
@@ -0,0 +1,1 @@
+console.log("Hello Beginner. How are you?");
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: './dist/app.bundle.js'
+  }
+};
+
